5ebef6645acce907713954b848a3ef5cd86a3c11
Upendra Kumar <upendra.k14@iiits.in>
2019-07-20 16:38:17 +0530

    Adds monolingual filter from original code

---
+++ mono/0-do-it-all.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+# Parameters:
+#	* directory that contains multiple monolingual files
+#	* language code
+#	* truecase model
+#	* subword unit model
+
+dir=$1
+src=$2
+tcmodel=$3
+bpemodel=$4
+
+mkdir $dir/output
+
+cat $dir/* > $dir/output/corpus.$src
+
+
+./1-unique.sh \
+    $dir/output/corpus.$src \
+	$src
+
+./2-identify-language.sh \
+    $dir/output/corpus.$src.unique.nonalpha \
+    $src
+
+
---
+++ mono/3-moses-scripts-subword-nmt.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+# Requires scripts from Moses - https://github.com/moses-smt/mosesdecoder
+# And Subword NMT - https://github.com/rsennrich/subword-nmt (pip install subword-nmt)
+# 
+# Prarameters:
+#	* directory where the previously processed (corpus.en.unique.nonalpha.goodlang)
+#	* language code
+#	* truecase model
+#	* subword unit model
+
+dir=$1
+src=$2
+tcmodel=$3
+bpemodel=$4
+
+mosesdir=/mnt/c/Users/Matiss/Desktop/mosesdecoder/scripts
+
+mkdir $dir/1-tok
+mkdir $dir/2-clean
+mkdir $dir/3-tc
+mkdir $dir/4-bpe
+
+# Tokenize & stuff...
+cat $dir/output/corpus.$src.unique.nonalpha.goodlang | \
+	$mosesdir/tokenizer/normalize-punctuation.perl -l $src | \
+	$mosesdir/tokenizer/tokenizer.perl -a -threads 8 -l $src \
+	> $dir/1-tok/corpus.tok.$src
+
+# Clean
+$mosesdir/training/clean-corpus-n.perl -ratio 3 $dir/1-tok/corpus.tok $src $src $dir/2-clean/corpus.clean.tok 2 80
+
+
+# Truecase
+$mosesdir/recaser/truecase.perl -model $tcmodel < $dir/2-clean/corpus.clean.tok.$src > $dir/3-tc/corpus.tc.$src
+
+# Split into subword units
+
+subword-nmt apply-bpe -c $bpemodel < $dir/3-tc/corpus.tc.$src > $dir/4-bpe/corpus.bpe.$src
